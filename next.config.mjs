f8e9ec342ab73f4dd8845ff9ecf4a68d727121cf
kenji <kenjiginjo@gmail.com>
2024-02-24 07:35:00 +0800

    Update .gitignore, add .env.example, modify tsconfig.json, and next.config.mjs, update package.json

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,23 @@
+import CopyPlugin from "copy-webpack-plugin";
+import path from "path";
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.plugins.push(
+        new CopyPlugin({
+          patterns: [
+            {
+              from: "db/stock.csv",
+              to: "db/stock.csv",
+            },
+          ],
+        })
+      );
+    }
+
+    return config;
+  },
+};
 
 export default nextConfig;
